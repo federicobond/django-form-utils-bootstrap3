cf1b8378b1a47bccbda96a4fe700561a569435fe
Federico Bond <federicobond@gmail.com>
2015-12-17 02:58:45 -0300

    Release 0.1.1

--- form_utils_bootstrap3/__init__.py
+++ form_utils_bootstrap3/__init__.py
@@ -5,6 +5,6 @@ Render form fieldsets with Bootstrap 3 markup via django-form-utils and
 django-bootstrap3
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Federico Bond <federicobond@gmail.com>'
 __all__ = []
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="django-form-utils-bootstrap3",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/federicobond/django-form-utils-bootstrap3",
 
     author="Federico Bond",
